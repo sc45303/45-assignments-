07b11d1bf3489112b71fb07d716a677357826a3c
sc45303 <162912378+sc45303@users.noreply.github.com>
2024-04-16 05:23:57 -0700

    Add files via upload

---
+++ personal-message.js
@@ -0,0 +1,2 @@
+var personName = "Hello MR,NANO would you like to come my home today";
+console.log(personName);
---
+++ personal-message.ts
@@ -0,0 +1,2 @@
+let personName:string = "Hello MR,NANO would you like to come my home today";
+console.log(personName);
